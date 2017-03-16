3308fba7a0e5d3ac5d208e37861ffff8352f4e84
David Weldon <dweldon@gmail.com>
2017-03-15 21:11:51 -0700

    Add simple server

---
+++ server.js
@@ -0,0 +1,16 @@
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello() {
+    return 'Hello world!';
+  },
+};
+
+graphql(schema, '{ hello }', root)
+  .then(res => console.log(res));

f392ae83413b0bb81e107c62c7e04a005e83b541
David Weldon <dweldon@gmail.com>
2017-03-17 17:00:15 -0700

    Use apollo server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 const schema = require('./schema');
 
 const PORT = 5000;
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(PORT);
 
 console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
